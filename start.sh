c0286b78881181703dd89606bea453c0d224738a
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-19 19:26:55 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-sudo echo -e "$KEY" > API_REST_SSH_KEY.pem
-sudo ssh -i "API_REST_SSH_KEY.pem" ubuntu@ec2-54-236-33-206.compute-1.amazonaws.com 'sh api-rest/desplegar.sh'
-sudo rm API_REST_SSH_KEY.pem
+echo -e "$KEY" > API_REST_SSH_KEY.pem
+ssh ubuntu@ec2-54-236-33-206.compute-1.amazonaws.com 'sh api-rest/desplegar.sh' YES
+rm API_REST_SSH_KEY.pem
