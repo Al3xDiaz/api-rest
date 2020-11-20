14301c50c85eb081945976cb6b4d5c7c546c2efe
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-19 19:54:06 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 mkdir ~/.ssh 
 echo "$KEY" > ~/.ssh/id_rsa.pub
-cat ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub echo "hola"
 ssh ubuntu@ec2-54-236-33-206.compute-1.amazonaws.com 'sh api-rest/desplegar.sh'
-rm .ssh/id_rsa.pub
+rm ~/.ssh/id_rsa.pub
