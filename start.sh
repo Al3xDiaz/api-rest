c0679d4148afb3dbabbee6f6188515fd2e2e3ea7
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-19 19:29:26 -0600

    fix CI/CD

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-echo -e "$KEY" > API_REST_SSH_KEY.pem
-ssh ubuntu@ec2-54-236-33-206.compute-1.amazonaws.com 'sh api-rest/desplegar.sh' YES
-rm API_REST_SSH_KEY.pem
+ssh ubuntu@ec2-54-236-33-206.compute-1.amazonaws.com 'sh api-rest/desplegar.sh'
