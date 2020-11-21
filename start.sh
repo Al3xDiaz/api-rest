1fae8b8f6bccda31f8a07ab062c657a137e1e9b2
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-20 22:43:43 -0600

    try 535

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@ cd ~
 mkdir .ssh
 touch .ssh/known_hosts
 echo "Deploy..."
-sudo echo -e $key> key.pem
+echo -e $key> .ssh/key.pem
 ssh-keyscan -t ecdsa $DEPLOY_HOST >> ~/.ssh/known_hosts
+cd .ssh
 ssh -i "key.pem" -o StrictHostKeyChecking=yes $DEPLOY_USER@$DEPLOY_HOST "sh ~/api-rest/desplegar.sh"
+rm key.pem
 #rm -r .ssh
 echo "Deploy success!!! you are awesome."
