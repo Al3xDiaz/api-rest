4055368eb8e64bc2a219532336793edefaea1162
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-20 21:45:20 -0600

    try 532

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-touch .ssh/known_hosts
+mkdir ~/.ssh
+touch ~/.ssh/known_hosts
 echo "Deploy..."
 echo -e $key> key
 ssh -i "key" -o StrictHostKeyChecking=yes $DEPLOY_USER@$DEPLOY_HOST "sh ~/api-rest/desplegar.sh"
