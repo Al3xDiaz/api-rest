8d8ab52f12ba6dad676dc75245919684138fce85
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-20 22:09:12 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ cd ~
 mkdir .ssh
 touch .ssh/known_hosts
 echo "Deploy..."
-echo -e $key> key.pem
+sudo echo -e $key> key.pem
 ssh-keyscan -t ecdsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 ssh -i "key.pem" -o StrictHostKeyChecking=yes $DEPLOY_USER@$DEPLOY_HOST "sh ~/api-rest/desplegar.sh"
 #rm -r .ssh
