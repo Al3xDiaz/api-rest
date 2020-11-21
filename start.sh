fcc316c0439486ed89c2e7dd98d21f48cff24fe8
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-20 23:01:24 -0600

    try 540
    Co-Authored-By: Keyci <47374214+keyci1113@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -2,11 +2,12 @@ cd ~
 mkdir .ssh
 touch .ssh/known_hosts
 echo "Deploy..."
-echo -e $key> .ssh/key
+echo -e $key> .ssh/key.pem
 ssh-keyscan -t ecdsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 cd .ssh
-chmod 400 key
-ssh -i "key" -o StrictHostKeyChecking=yes $DEPLOY_USER@$DEPLOY_HOST "sh ~/api-rest/desplegar.sh"
+chmod 400 key.pem
+cat key.pem
+ssh -i "key.pem" -o StrictHostKeyChecking=yes $DEPLOY_USER@$DEPLOY_HOST "sh ~/api-rest/desplegar.sh"
 #rm key
 #rm -r .ssh
 echo "Deploy success!!! you are awesome."
