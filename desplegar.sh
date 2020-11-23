d5ba8a8766f720da93e576ee636798bfa58ef56b
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 18:12:28 -0600

    test args sh

--- desplegar.sh
+++ desplegar.sh
@@ -4,4 +4,3 @@ cd api-rest/
 sudo docker rmi $(sudo docker images -a -q)
 sudo docker build -t api-content .
 sudo docker kill $(sudo docker ps -a | sudo grep "api-content")
-sudo docker run -d -p 80:80 api-content
