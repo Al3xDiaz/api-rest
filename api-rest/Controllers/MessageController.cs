74bca9816b28f2a3a9dcb969a541b24ea08e3c46
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 19:18:39 -0600

    fix CI

--- api-rest/Controllers/MessageController.cs
+++ api-rest/Controllers/MessageController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace api_rest.Controllers
 {
@@ -12,9 +7,9 @@ namespace api_rest.Controllers
     public class MessageController : ControllerBase
     {
         [HttpGet]
-        public IActionResult getTest()
+        public string getTest()
         {
-            return Ok("i lava you!!!");
+            return "i lava you!!!";
         }
     }
 }
--- desplegar.sh
+++ desplegar.sh
@@ -1,7 +1,7 @@
-cd api-rest/
+cd ~/api-rest/
 sudo git checkout .
 sudo git pull
-cd api-rest/
+cd ~/api-rest/api-rest/
 sudo docker rmi $(sudo docker images -a -q)
 sudo docker build -t api-content .
 sudo docker kill $(sudo docker ps -a | sudo grep "api-content")
