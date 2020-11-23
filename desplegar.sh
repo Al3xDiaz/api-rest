c647e180420d4c99c409500eecb07f101d1c8718
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 20:49:16 -0600

    Update desplegar.sh

--- desplegar.sh
+++ desplegar.sh
@@ -1,7 +1,8 @@
 cd ~/api-rest/
 cd api-rest/
 sudo docker build -t api-content .
-containerID=$(sudo docker container ls | grep "api-content" | awk '{print $1}')
+containerID=$(sudo docker container ls | sudo grep "api-content" | sudo awk '{print $1}')
+echo $containerID
 sudo docker stop $containerID
 sudo docker rm $containerID
 sudo docker run -d -p 80:80 api-content
