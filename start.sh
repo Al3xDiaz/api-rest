743a38067ae3ea15d7a2b5afb8993b85dfc969c6
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 19:47:20 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "$key"> .ssh/key.pem
 ssh-keyscan -t ecdsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 cd .ssh
 chmod 400 key.pem
-ssh -i "key.pem"  $DEPLOY_USER@$DEPLOY_HOST "cd ~/api-rest/;sh desplegar.sh; sudo git checkout .;sudo git pull"
+ssh -i "key.pem"  $DEPLOY_USER@$DEPLOY_HOST "cd ~/api-rest/;sudo git checkout .;sudo git pull;sh desplegar.sh"
 rm key.pem
 #rm -r .ssh
 echo "Deploy success!!! you are awesome."
