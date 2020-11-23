fe665aa16d6e4e3283c80b175b027f5334f8ea3f
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 19:55:38 -0600

    Update desplegar.sh

--- desplegar.sh
+++ desplegar.sh
@@ -1,7 +1,6 @@
 cd ~/api-rest/
 cd api-rest/
 sudo docker build -t api-content .
-sudo docker kill $(sudo docker ps -a | sudo grep "api-content")
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
 sudo docker run -d -p 80:80 api-content
-sudo docker rm $(sudo docker ps -a -f status=exited -q)
-sudo docker rmi $(sudo docker images -a -q)
