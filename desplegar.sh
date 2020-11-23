224fac4681e0649567d530a2e2a42dd1cf404970
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 19:38:32 -0600

    fix CI/CD

--- desplegar.sh
+++ desplegar.sh
@@ -1,7 +1,7 @@
-sudo git checkout .
-sudo git pull
-cd ~/api-rest/api-rest/
-sudo docker rmi $(sudo docker images -a -q)
+cd ~/api-rest/
+cd api-rest/
 sudo docker build -t api-content .
 sudo docker kill $(sudo docker ps -a | sudo grep "api-content")
 sudo docker run -d -p 80:80 api-content
+sudo docker rm $(sudo docker ps -a -f status=exited -q)
+sudo docker rmi $(sudo docker images -a -q)
--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "$key"> .ssh/key.pem
 ssh-keyscan -t ecdsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 cd .ssh
 chmod 400 key.pem
-ssh -i "key.pem"  $DEPLOY_USER@$DEPLOY_HOST "sh ~/api-rest/desplegar.sh"
+ssh -i "key.pem"  $DEPLOY_USER@$DEPLOY_HOST "cd ~/api-rest/;sh desplegar.sh; sudo git checkout .;sudo git pull"
 rm key.pem
 #rm -r .ssh
 echo "Deploy success!!! you are awesome."
