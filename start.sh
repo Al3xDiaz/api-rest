df5201ce288014ea023b1031848f26ef8b55a1d0
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 18:20:55 -0600

    fix CI/CD

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "$key"> .ssh/key.pem
 ssh-keyscan -t ecdsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 cd .ssh
 chmod 400 key.pem
-ssh -i "key.pem"  $DEPLOY_USER@$DEPLOY_HOST "sh ~/api-rest/desplegar.sh; $1"
+ssh -i "key.pem"  $DEPLOY_USER@$DEPLOY_HOST "sh ~/api-rest/desplegar.sh"
 rm key.pem
 #rm -r .ssh
 echo "Deploy success!!! you are awesome."
