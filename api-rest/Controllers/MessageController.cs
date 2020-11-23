eac99659e2394ae479aa2c96555284282f56b12b
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 18:59:12 -0600

    add endpoint message

---
+++ api-rest/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_rest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult getTest()
+        {
+            return Ok("i lava you!!!");
+        }
+    }
+}
