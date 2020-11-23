06d4cfdf0bf72992f3deb061778b2b8a89bbdcba
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-23 09:53:29 -0600

    change port 80 to port 81

--- desplegar.sh
+++ desplegar.sh
@@ -1,9 +1,9 @@
 cd ~/api-rest/
 cd api-rest/
 sudo docker build -t api-content .
-containerID=$(sudo docker container ls | sudo grep "80" | sudo awk '{print $1}')
+containerID=$(sudo docker container ls | sudo grep "81" | sudo awk '{print $1}')
 echo $containerID
 sudo docker stop $containerID
 sudo docker rm $containerID
-sudo docker run -d -p 80:80 api-content
+sudo docker run -d -p 81:81 api-content
 sudo docker system prune -a --force
