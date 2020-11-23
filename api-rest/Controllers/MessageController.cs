914b9ba1c14cfbe0d604d688d20f8f7fb61a5e02
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-22 20:08:37 -0600

    Update MessageController.cs

--- api-rest/Controllers/MessageController.cs
+++ api-rest/Controllers/MessageController.cs
@@ -9,7 +9,7 @@ namespace api_rest.Controllers
         [HttpGet("{name}")]
         public string getTest(string name)
         {
-            return $"i lava you!!!, my kuroneko :3, te adoro {name}";
+            return $"hello {name}!!! welcome test ;)";
         }
     }
 }
