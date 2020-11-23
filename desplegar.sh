e814473c9d7d34b213536d2042affb26ce1be2bf
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-11-23 11:36:38 -0600

    Update desplegar.sh

--- desplegar.sh
+++ desplegar.sh
@@ -1,9 +1,9 @@
 cd ~/api-rest/
 cd api-rest/
-docker build -t api-content .
-containerID=$(docker container ls | grep "0.0.0.0:5000" | awk '{print $1}')
+sudo docker build -t api-content .
+containerID=$(sudo docker container ls | grep "0.0.0.0:5000" | awk '{print $1}')
 echo $containerID
-docker stop $containerID
-docker rm $containerID
-docker run -d -p 5000:80 api-content
-docker system prune -a --force
+sudo docker stop $containerID
+sudo docker rm $containerID
+sudo docker run -d -p 5000:80 api-content
+sudo docker system prune -a --force
