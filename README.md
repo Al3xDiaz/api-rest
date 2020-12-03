7672cfb39864ffa9cb31a0d49da09a4924b3699f
Alex Leonel Diaz <47805592+Al3xDiaz@users.noreply.github.com>
2020-12-02 21:14:00 -0600

    change port

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Portafolio
 Esto es un API hecho con C# .net core
 ## Hosting
-[Endpoint](http://ec2-54-236-33-206.compute-1.amazonaws.com:443/Message/Alex)
+[Endpoint](http://ec2-54-236-33-206.compute-1.amazonaws.com/Message/Alex)
--- desplegar.sh
+++ desplegar.sh
@@ -1,8 +1,8 @@
 cd ~/api-rest/api-rest
 sudo docker build -t api-content .
-containerID=$(sudo docker container ls | sudo grep "0.0.0.0:443" | sudo awk '{print $1}')
+containerID=$(sudo docker container ls | sudo grep "0.0.0.0:80" | sudo awk '{print $1}')
 echo $containerID
 sudo docker stop $containerID
 sudo docker rm $containerID
-sudo docker run -d -p 443:80 api-content
+sudo docker run -d -p 80:80 api-content
 sudo docker system prune -a --force
